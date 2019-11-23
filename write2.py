0c53f7c6218d2a7d9963346238aef150e5f3e84b
antu <antu0124@gmail.com>
2019-11-23 19:05:43 +1100

    comment from python script

--- write2.py
+++ write2.py
@@ -38,22 +38,21 @@ else:
 #Repository.index.commit(commit_message)
 #origin = repo.remote('origin')
 
-PATH_OF_GIT_REPO = r'/home/frahman/python/'  # make sure .git folder is properly configured
+PATH_OF_GIT_REPO = r'/home/frahman/python/.git/'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'comment from python script'
 
 def git_push():
-    try:
-        repo = Repo(PATH_OF_GIT_REPO)
-        print(repo)
-        #file_list = [
-        #'report.txt'
-        #]
-        repo.git.add(update=True)
-        #repo.git.add(file_list)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='master')
-        print(origin)
-        origin.push()
-    except:
-        print('Some error occured while pushing the code')
+#try:
+    repo = Repo(PATH_OF_GIT_REPO)
+    print(repo)
+    file_list = [
+    'report.txt'
+    ]
+    repo.git.add(update=True)
+    repo.git.add(file_list)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='master')
+    origin.push()
+    #except:
+     #   print('Some error occured while pushing the code')
 git_push()    
