665175f9c14a65a5b6fa4ea4979e8de60c1d7405
antu <antu0124@gmail.com>
2019-12-11 23:24:35 +1100

    comment from python script

--- push.py
+++ push.py
@@ -1,4 +1,4 @@
-import os
+
 import sys
 import git
 import github
@@ -7,18 +7,20 @@ from github import Github
 from git import Repo
 
 PATH_OF_GIT_REPO = r'/home/frahman/python/.git/'  # make sure .git folder is properly configured
-#PATH_OF_GIT_REPO = os.path.abspath('.git/')
 COMMIT_MESSAGE = 'comment from python script'
 
-repo = Repo(PATH_OF_GIT_REPO)
-print(repo)
-file_list = [
-'report.txt'
-]
-repo.git.add(update=True)
-repo.git.add()
-repo.index.commit(COMMIT_MESSAGE)
-origin = repo.remote(name='origin')
-origin.push()
+def git_push():
+#try:
+    repo = Repo(PATH_OF_GIT_REPO)
+    print(repo)
+    #file_list = [
+    #'report.txt'
+    #]
+    repo.git.add(update=True)
+    repo.git.add()
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='origin')
+    origin.push()
     #except:
      #   print('Some error occured while pushing the code')
+git_push()    
